de685a5314fd3a13dcd1836c6217a67aa0fc816e
edward <edwardlfish@gmail.com>
2024-11-18 21:05:10 +0200

    Simple routing

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,23 +2,20 @@ import Home from "./Home.tsx";
 import AboutMe from "./AboutMe.tsx";
 import StarWars from "./StarWars.tsx";
 import Contact from "./Contact.tsx";
+import {Route, Routes} from "react-router-dom";
 import {navItems} from "../utils/constants.ts";
-import {useContext} from "react";
-import {SWContext} from "../utils/context.ts";
 
 const Main = () => {
-    const {page} = useContext(SWContext);
 
-    switch (page) {
-        case navItems[1]:
-            return <AboutMe/>;
-        case navItems[2]:
-            return <StarWars/>;
-        case navItems[3]:
-            return <Contact/>;
-        default:
-            return <Home/>;
-    }
+    return (
+        <Routes>
+            <Route path={'/'} element={<Home/>}/>
+            <Route path={`/${navItems[0]}`} element={<Home/>}/>
+            <Route path={`/${navItems[1]}`} element={<AboutMe/>}/>
+            <Route path={`/${navItems[2]}`} element={<StarWars/>}/>
+            <Route path={`/${navItems[3]}`} element={<Contact/>}/>
+        </Routes>
+    )
 }
 
 export default Main;
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,15 +1,13 @@
-import {useContext} from "react";
-import {SWContext} from "../utils/context.ts";
-
 interface Props {
     itemTitle: string
 }
 
 const NavItem = ({itemTitle}: Props) => {
-    const {changePage} = useContext(SWContext);
 
     return (
-        <li onClick={() => changePage(itemTitle)} className={`bg-red-color border-black border-2 rounded-[5px] px-3 py-2 cursor-pointer hover:text-white hover:bg-red-500`}>{itemTitle}</li>
+        <li className={`bg-red-color border-black border-2 rounded-[5px] px-3 py-2 cursor-pointer hover:text-white hover:bg-red-500`}>
+            <a href={`/${itemTitle}`}>{itemTitle}</a>
+        </li>
     );
 };
 
