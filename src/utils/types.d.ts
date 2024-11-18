d7cbfb8e22128cea720d1f9514d0d9c038dd1064
edward <edwardlfish@gmail.com>
2024-11-18 19:57:19 +0200

    First commit

---
+++ src/utils/types.d.ts
@@ -0,0 +1,15 @@
+export interface SWContextValue {
+    page: string,
+    changePage: (page: string) => void,
+}
+
+export interface HeroInfo {
+    name: string,
+    gender: string,
+    birth_year: string,
+    height: number,
+    mass: number,
+    hair_color: string,
+    skin_color: string,
+    eye_color: string
+}
