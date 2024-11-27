7342037a275ae0aa4294d9a3bb3559fe2662355c
edward <edwardlfish@gmail.com>
2024-11-27 23:19:23 +0200

    apply hoc withErrorPage

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,21 +1,10 @@
-import {useContext, useEffect, useState} from "react";
-import {base_url, characters, defaultHero, period_month} from "../utils/constants.ts";
-import {useParams} from "react-router-dom";
-import {SWContext} from "../utils/context.ts";
-import ErrorPage from "./ErrorPage.tsx";
+import {useEffect, useState} from "react";
+import {base_url, period_month} from "../utils/constants.ts";
+import {withErrorPage} from "../hoc/withErrorPage.tsx";
 
 const Contact = () => {
     const [planets, setPlanets] = useState(['wait...']);
 
-    const {heroId = defaultHero} = useParams();
-    const {changeHero} = useContext(SWContext);
-
-    useEffect(() => {
-        if (!characters[heroId]) {
-            return;
-        }
-        changeHero(heroId);
-    }, [heroId])
 
     async function fillPlanets(url: string) {
         const response = await fetch(url);
@@ -37,36 +26,35 @@ const Contact = () => {
         }
     }, [])
 
-    return characters[heroId] ? (
-            <form className={`w-4/5 my-0 mx-auto rounded-[5px] bg-[#f2f2f2] p-5`} onSubmit={(e) => {
-                e.preventDefault();
-            }}>
-                <label className={`w-full text-red-color`}>First Name
-                    <input className={`border w-full p-3 border-[#ccc] rounded-[4px] mt-1.5 mb-4 resize-y`} type="text"
-                           name="firstname" placeholder="Your first name..."/>
-                </label>
-                <label className={`w-full text-red-color`}>Last Name
-                    <input className={`border w-full p-3 border-[#ccc] rounded-[4px] mt-1.5 mb-4 resize-y`} type="text"
-                           name="lastname" placeholder="Your last name..."/>
-                </label>
-                <label className={`w-full text-red-color`}>Planet
-                    <select className={`border w-full text-black p-3 border-[#ccc] rounded-[4px] mt-1.5 mb-4 resize-y`}
-                            name="planet">{
-                        planets.map(item => <option value={item} key={item}>{item}</option>)
-                    }
-                    </select>
-                </label>
-                <label className={`w-full text-red-color`}>Subject
-                    <textarea className={`border h-52 w-full p-3 border-[#ccc] rounded-[4px] mt-1.5 mb-4 resize-y`}
-                              name="subject" placeholder="Write something..."/>
-                </label>
-                <button
-                    className={`bg-[#4CAF50] text-white py-3 px-5 border-none rounded-[4px] cursor-pointer hover:bg-[#45a049]`}
-                    type="submit">Submit
-                </button>
-            </form>
-        )
-        : <ErrorPage/>
+    return (
+        <form className={`w-4/5 my-0 mx-auto rounded-[5px] bg-[#f2f2f2] p-5`} onSubmit={(e) => {
+            e.preventDefault();
+        }}>
+            <label className={`w-full text-red-color`}>First Name
+                <input className={`border w-full p-3 border-[#ccc] rounded-[4px] mt-1.5 mb-4 resize-y`} type="text"
+                       name="firstname" placeholder="Your first name..."/>
+            </label>
+            <label className={`w-full text-red-color`}>Last Name
+                <input className={`border w-full p-3 border-[#ccc] rounded-[4px] mt-1.5 mb-4 resize-y`} type="text"
+                       name="lastname" placeholder="Your last name..."/>
+            </label>
+            <label className={`w-full text-red-color`}>Planet
+                <select className={`border w-full text-black p-3 border-[#ccc] rounded-[4px] mt-1.5 mb-4 resize-y`}
+                        name="planet">{
+                    planets.map(item => <option value={item} key={item}>{item}</option>)
+                }
+                </select>
+            </label>
+            <label className={`w-full text-red-color`}>Subject
+                <textarea className={`border h-52 w-full p-3 border-[#ccc] rounded-[4px] mt-1.5 mb-4 resize-y`}
+                          name="subject" placeholder="Write something..."/>
+            </label>
+            <button
+                className={`bg-[#4CAF50] text-white py-3 px-5 border-none rounded-[4px] cursor-pointer hover:bg-[#45a049]`}
+                type="submit">Submit
+            </button>
+        </form>
+    )
 }
 
-export default Contact;
+export default withErrorPage(Contact);
---
+++ src/hoc/withErrorPage.tsx
@@ -0,0 +1,19 @@
+import {useParams} from "react-router-dom";
+import {characters, defaultHero} from "../utils/constants.ts";
+import {useContext, useEffect} from "react";
+import {SWContext} from "../utils/context.ts";
+import ErrorPage from "../components/ErrorPage.tsx";
+
+export const withErrorPage = (Component: React.FC) => (props: object) => {
+    const {hero: heroId = defaultHero} = useParams();
+    const {changeHero} = useContext(SWContext);
+
+    useEffect(() => {
+        if (characters[heroId]) {
+            changeHero(heroId);
+        }
+
+    }, [heroId])
+
+    return characters[heroId] ? <Component {...props}/> : <ErrorPage/>;
+}
