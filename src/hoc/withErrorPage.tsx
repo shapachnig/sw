7eb9c109325bdca7c80e922399fc0c5f70a75718
edward <edwardlfish@gmail.com>
2024-11-28 18:18:44 +0200

    change type to ComponentType

--- src/hoc/withErrorPage.tsx
+++ src/hoc/withErrorPage.tsx
@@ -1,10 +1,10 @@
 import {useParams} from "react-router-dom";
 import {characters, defaultHero} from "../utils/constants.ts";
-import {useContext, useEffect} from "react";
+import {ComponentType, useContext, useEffect} from "react";
 import {SWContext} from "../utils/context.ts";
 import ErrorPage from "../components/ErrorPage.tsx";
 
-export const withErrorPage = (Component: React.FC) => (props: object) => {
+export const withErrorPage = (Component: ComponentType) => (props: object) => {
     const {heroId = defaultHero} = useParams();
     const {changeHero} = useContext(SWContext);
 
